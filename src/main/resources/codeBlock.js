67429b9944723c73bebd0653f2f0a59cfe579bfe
rcorbish <rcorbish@outlook.com>
2016-04-17 20:48:15 -0400

    Updated technique for merging blocks

--- src/main/resources/codeBlock.js
+++ src/main/resources/codeBlock.js
@@ -11,91 +11,50 @@ function CodeBlocks() {
 	this.joinedToEdgeOf = function(codeBlock) {
 		// Is the codeBlock underneath any another block ?
 		var rc = {} ;
-		
+
 		for (var i = 0; i < this.codeBlocks.length; i++) {
 			var testCodeBlock = this.codeBlocks[i];
 			if( codeBlock === testCodeBlock ) continue ;   // can't join to self don't test
 			if( testCodeBlock.following === codeBlock ) continue ;	// if we're already joined to the block don't test
 			// if we're at the same x and the top of the input test block is at the bottom of another block...
 			if (Math.abs(codeBlock.x - testCodeBlock.x) < 5
-				&& Math.abs(codeBlock.y - (testCodeBlock.y + testCodeBlock.height - 7)) < 5) {
-				rc.top = testCodeBlock.name ;
+					&& Math.abs(codeBlock.y - (testCodeBlock.y + testCodeBlock.height - 7)) < 5) {
+				rc.top = testCodeBlock ;
 			}			
 			// if we're at the same y and the top of the input test block is at the right of another block...
 			if (Math.abs(codeBlock.y - testCodeBlock.y) < 5
-				&& Math.abs(codeBlock.x - (testCodeBlock.x + testCodeBlock.width - 7)) < 5) {
-				rc.left = testCodeBlock.name ;
+					&& Math.abs(codeBlock.x - (testCodeBlock.x + testCodeBlock.width - 7)) < 5) {
+				rc.left = testCodeBlock ;
 			}			
 			// if we're at the same y and the top of the input test block is at the left of another block...
 			if (Math.abs(codeBlock.y - testCodeBlock.y) < 5
-				&& Math.abs(testCodeBlock.x - (codeBlock.x + codeBlock.width - 7)) < 5) {
-				rc.right = testCodeBlock.name ;
+					&& Math.abs(testCodeBlock.x - (codeBlock.x + codeBlock.width - 7)) < 5) {
+				rc.right = testCodeBlock ;
 			}			
 
 			if (Math.abs(codeBlock.x - testCodeBlock.x) < 5
-				&& Math.abs(testCodeBlock.y - (codeBlock.y + codeBlock.effectiveHeight - 7)) < 5) {
-				rc.bottom = testCodeBlock.name ;
-			}
-
-		}
-/*
-		// Is the codeblock immediately above another block
-		// we need to move to the bottom of the chain of joined blocks for this test
-		var cb = codeBlock ;
-		while( cb.following ) cb=cb.following ;  // find bottom block in chain to match
-
-		for (var i = 0; i < this.codeBlocks.length; i++) {
-			var testCodeBlock = this.codeBlocks[i];
-			if( codeBlock === testCodeBlock ) continue ;   // can't join to self don't test
-			if( cb.following === testCodeBlock ) continue ; // already joined ? - ignore this test
-			
-			if (Math.abs(codeBlock.x - testCodeBlock.x) < 5
-				&& Math.abs(testCodeBlock.y - (cb.y + cb.height - 7)) < 5) {
-				rc.bottom = testCodeBlock.name ;
+					&& Math.abs(testCodeBlock.y - (codeBlock.y + codeBlock.effectiveHeight - 7)) < 5) {
+				rc.bottom = testCodeBlock ;
 			}
 		}
-
-
-*/
-		
 		return rc ;
 	}
 
-	this.testJoin = function(codeBlock) {
+	this.joinBlock  = function(codeBlock) {
+		var joinInfo = this.joinedToEdgeOf( codeBlock ) ;
 
-		if ( !codeBlock.start ) {
-			for (var i = 0; i < this.codeBlocks.length; i++) {
-				var testCodeBlock = this.codeBlocks[i];
-				if( testCodeBlock.following !== codeBlock && !testCodeBlock.end ) {
-					if (Math.abs(codeBlock.x - testCodeBlock.x) < 5
-							&& Math
-							.abs(codeBlock.y
-									- (testCodeBlock.y
-											+ testCodeBlock.height - 7)) < 5) {
-						codeBlock.joinBelow( testCodeBlock ) ;
-						return;
-					}
-				}
-			}
-		}
-		// Joining by moving above a block
-		var cb = codeBlock ;
-		while( cb.following ) cb=cb.following ;  // find bottom block in chain to match
+		if( joinInfo.top ) {
+			console.log( "Join", joinInfo.top.name, "above", codeBlock.name ) ;
+			codeBlock.joinBelow( joinInfo.top ) ;
+		} else if( joinInfo.bottom ) {
+			console.log( "Join", joinInfo.bottom.name, "below", codeBlock.name ) ;
+			var endOfChain = codeBlock ;
+			while( endOfChain.following ) endOfChain=endOfChain.following ;
 
-		if( !codeBlock.end ) {
-			for (var i = 0; i < this.codeBlocks.length; i++) {
-				var testCodeBlock = this.codeBlocks[i];
-				if( cb.following !== testCodeBlock ) {
-					if (Math.abs(codeBlock.x - testCodeBlock.x) < 5
-							&& Math
-							.abs(testCodeBlock.y - (cb.y + cb.height - 7)) < 5) {
-						testCodeBlock.joinBelow( cb ) ;
-						return;
-					}
-				}
-			}
+			joinInfo.bottom.joinBelow( endOfChain ) ;
 		}
-	} ;
+	}
+
 
 	this.draw = function( ctx ) {
 		for (var i = 0; i < this.codeBlocks.length; i++) {
@@ -108,8 +67,8 @@ function CodeBlocks() {
 
 	this.codeBlockAt = function( x,y ) {
 		var rc = null;
-		for (var i = 0; i < this.codeBlocks.length; i++) {
-			var codeBlock = this.codeBlocks[i];
+		for (var i = this.codeBlocks.length; i>0 ; i--) {
+			var codeBlock = this.codeBlocks[i-1];
 
 			if (x > codeBlock.x
 					&& x<codeBlock.x+codeBlock.width && y>codeBlock.y
@@ -131,7 +90,7 @@ function CodeBlock( args ) {
 	this.end = this.type === 'end' ;
 	this.numInputs = args.numInputs || 0 ;
 	this.output = args.output || false ;
-	this.height = args.height || ( 30 + ( 15 * this.numInputs ) ) ;
+	this.height = 20 * ( 2 + Math.max( 0, ( this.numInputs - 1 ) ) ) ;
 	this.width = args.width || 300 ;
 	this.effectiveHeight = this.height ;
 	this.following = null,
@@ -143,30 +102,59 @@ function CodeBlock( args ) {
 			this.effectiveHeight += this.following.calcHeight() - 5 ;
 		}
 		return this.effectiveHeight;
-	}
-
+	} ;
 
 	this.joinBelow = function( above ) {
-		this.parent = above;
-		if( above.following ) {
-			var cb = this ;
-			while( cb.following ) cb = cb.following ;
-			above.following.joinBelow( cb ) ;
+		var currentChild = above.following ;
+		if( currentChild ) {
+			currentChild.joinBelow( this ) ;
 		}
-		above.following = this ;
+		this.setParent( above ) ;
+
 		var ultimateParent = above;
 		while (ultimateParent.parent) {
 			ultimateParent = ultimateParent.parent;
 		}
 		ultimateParent.calcHeight();
-	}
+	} ;
+
+	this.setParent = function( p ) {
+		if( !this.start ) { 
+			// if already have a parent - make sure parent doesn't think 
+			// I'm still part of the chain
+			if( this.parent ) {
+				this.parent.following = null ;
+				var up = this.parent ;
+				while( up.parent ) up = up.parent ;
+				up.calcHeight() ;			
+			}
+
+			// if parent has something else in the chain (that I am replacing)...
+			if( p ) {
+				if( p.following ) {
+					p.following.parent = this ;
+				}
+				if( !p.end ) {
+					p.following = this ;  // other part of main code
+					this.parent = p ;
+				} else {
+					p.following = null ;
+					this.parent = null ;
+					this.y = p.y+p.height+15 ;
+					this.x += 15 ;
+				}
+			} else {
+				this.parent = p ;	// Always set a null parent
+			}
+		}
+	} ;
 
 	this.draw = function ( ctx ) {
 		var poly;
-		
+
 		var lhs = this.output ? 
 				[ 5, 5, 5, this.height-15, 0, this.height-15, 0, this.height - 5, 5, this.height - 5,  5, this.height ] :
-				[ 5, 5, 5, this.height ] ;
+					[ 5, 5, 5, this.height ] ;
 				var bot = this.end ? 
 						[20, this.height, this.width, this.height ] :
 							[20, this.height, 20, this.height - 5, 30, this.height - 5, 30, this.height, this.width, this.height ] ;
@@ -175,7 +163,7 @@ function CodeBlock( args ) {
 									[ this.width, 5 ] ;
 								var top = this.start ? [ ] : [ 30, 5, 30, 0, 20, 0, 20, 5 ] ; 
 
-/*								
+								/*								
 								var lhs = this.output ? 
 										[ 7, 7, 7, this.height - 17, 2, this.height - 17, 2, this.height - 7, 7, this.height - 7,   7, this.height - 2 ] :
 											[ 7, 7, 7, this.height - 2 ] ;
@@ -186,7 +174,7 @@ function CodeBlock( args ) {
 														[ this.width - 2, this.height - 7, this.width - 7, this.height - 7,this.width - 7, this.height - 17 , this.width - 2, this.height - 17,this.width - 2, 7 ] :
 															[ this.width - 2, 7 ] ;
 														var top = this.start ? [ ] : [ 30, 7, 30, 2, 20, 2, 20, 7 ] ; 
-*/
+								 */
 								poly =  lhs.concat( bot, rhs, top ) ;
 
 								switch ( this.type ) {
